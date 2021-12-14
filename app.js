934d8450e63cec674c3f73c6c8b2b8aecbec1ba4
김동근 <kimdg0429@gmail.com>
2021-12-14 21:10:36 +0900

    indexRouter 연결

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+//라우터 목록
+const indexRouter = require("./routes/indexRouter");
 
 const app = express();
+// view 경로 설정
+app.set("views", __dirname + "/views");
 
-app.use(morgan('dev'));
+// 화면 구성 엔진을 ejs로 설정
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // 라우터 추가하기
+app.use("/", indexRouter);
 
 module.exports = app;
---
+++ routes/indexRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const router = Router();
+
+//템플릿 engine 저장 안한 경로: C:\Users\(사용자명)\Desktop\teamProject\project-template\routes
+router.get("/", (req, res) => {
+  res.render("index.html"); // views/index.html 파일을 렌더링
+  return;
+});
+
+module.exports = router;
