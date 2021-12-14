c68366103634401731267973c5977c0dfc2da8ec
김동근 <kimdg0429@gmail.com>
2021-12-14 16:46:23 +0900

    스키마 생성

---
+++ models/schemas/comment.js
@@ -0,0 +1,26 @@
+const { Schema } = require("mongoose");
+const reCommentSchema = require("./comment");
+
+const CommentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    reComments: [reCommentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = CommentSchema;
---
+++ models/schemas/re-comment.js
@@ -0,0 +1,24 @@
+const { Schema } = require("mongoose");
+
+const reCommentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = reCommentSchema;
