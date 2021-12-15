f71d58f34394697cc0a6b5fb8005493504fecadf
김동근 <kimdg0429@gmail.com>
2021-12-15 16:47:41 +0900

    kakaoCallbackRouter에서 db에 신규유저 추가

--- src/routes/kakaoCallbackRouter.js
+++ src/routes/kakaoCallbackRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const axios = require("axios");
 const qs = require("qs");
-
+const { User } = require("../models");
 const router = Router();
 
 require("dotenv").config();
@@ -41,13 +41,29 @@ router.get("/", async (req, res) => {
       url: "https://kapi.kakao.com/v2/user/me",
       headers: {
         Authorization: `Bearer ${token.data.access_token}`,
-      }, // 헤더에 내용을 보고 보내주겠다.
+      },
     });
   } catch (e) {
     res.json(e.data);
   }
 
   req.session.kakao = user.data;
+
+  const newUser = await User.findOne({
+    email: req.session.kakao.kakao_account.email,
+  });
+
+  if (!newUser) {
+    try {
+      await User.create({
+        email: req.session.kakao.kakao_account.email,
+        nickname: req.session.kakao.kakao_account.profile.nickname,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   res.redirect("/");
 });
 
