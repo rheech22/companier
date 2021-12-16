1e0ebdaefc4169cb2127b8d58efc5038c1513ca7
김동근 <kimdg0429@gmail.com>
2021-12-16 14:06:54 +0900

    각 기초페이지들 연결

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get("/", (req, res) => {
   if (!req.session.kakao) {
     //카카오 로그인을 안해서 세션에 없으면
-    res.render("index.html");
+    res.render("main.html");
   } else {
     //카카오 로그인을 해서 세션에 kakao가 존재하면
     res.render("kakaoLogin.html"); //
--- src/routes/postRouter/protectPostRouter/protectPostController.js
+++ src/routes/postRouter/protectPostRouter/protectPostController.js
@@ -1,4 +1,4 @@
-const { Post, User } = require('../../models');
+const { Post, User } = require("../../../models");
 
 const getPosts = async (req, res) => {
   const posts = await Post.find({});
@@ -16,15 +16,11 @@ const getPostDetail = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  const {
-    title,
-    content,
-    category,
-  } = req.body;
+  const { title, content, category } = req.body;
 
   try {
     // 테스트를 위해 유저 데이터를 임시로 특정
-    const user = await User.findOne({ nickname: 'CH' });
+    const user = await User.findOne({ nickname: "CH" });
     const post = await Post.create({
       title,
       content,
@@ -52,11 +48,7 @@ const deletePost = async (req, res) => {
 const updatePost = async (req, res) => {
   const { id } = req.params;
 
-  const {
-    title,
-    content,
-    category,
-  } = req.body;
+  const { title, content, category } = req.body;
 
   if (!title || !content || !category) {
     res.status(400).end();
@@ -64,11 +56,14 @@ const updatePost = async (req, res) => {
 
   // 추후에 요청자와 post 작성자가 일치하는지 검증하는 로직 추가해야 함
 
-  const post = await Post.updateOne({ _id: id }, {
-    title,
-    content,
-    category,
-  });
+  const post = await Post.updateOne(
+    { _id: id },
+    {
+      title,
+      content,
+      category,
+    }
+  );
 
   if (!post) res.status(404).end();
 
