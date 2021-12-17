9832b49ddf9324d188b7675eec1026aac7194b94
김동근 <kimdg0429@gmail.com>
2021-12-17 15:33:01 +0900

    반려 이야기 연결 및 페이지네이션 구현

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
     res.render("index.html");
   } else {
     //카카오 로그인을 해서 세션에 kakao가 존재하면
-    res.render("index.html", { data: "true" });
+    res.render("index.html", { isLogined: "true" });
   }
 });
 
--- src/routes/postRouter/myPetBoardRouter/myPetBoardController.js
+++ src/routes/postRouter/myPetBoardRouter/myPetBoardController.js
@@ -1,8 +1,36 @@
 const { Post, User } = require("../../../models");
 
 const getPosts = async (req, res) => {
-  const posts = await Post.find({});
-  res.json(posts);
+  const page = Number(req.query.page || 1); // url 쿼리에서 page 받기, 기본값 1
+  const perPage = Number(req.query.perPage || 12); // url 쿼리에서 peRage 받기, 기본값 12
+  const [total, posts] = await Promise.all([
+    Post.countDocuments({}),
+    Post.find({})
+      .sort({ createdAt: -1 })
+      .skip(perPage * (page - 1))
+      .limit(perPage)
+      .populate("author"),
+  ]);
+  const totalPage = Math.ceil(total / perPage);
+
+  if (!req.session.kakao) {
+    // 카카오 로그인을 안해서 세션에 없으면 isLogined는 제외하고 렌더링
+    res.render("myPetBoard.html", {
+      posts,
+      page,
+      perPage,
+      totalPage,
+    });
+  } else {
+    // 카카오 로그인을 해서 세션에 kakao가 존재하면 isLogined까지 렌더링(로그아웃, 로그인 구분)
+    res.render("myPetBoard.html", {
+      isLogined: "true",
+      posts,
+      page,
+      perPage,
+      totalPage,
+    });
+  }
 };
 
 const getPostDetail = async (req, res) => {
