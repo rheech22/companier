d0eed0f1964bcc64c72ef6cdb62771b0a32d8b02
Aiden <rheech22@gmail.com>
2021-12-18 12:11:34 +0900

    유저 상세 페이지 라우터 기본 로직 작성

--- src/routes/myPageRouter/index.js
+++ src/routes/myPageRouter/index.js
@@ -1,19 +1,26 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
 const router = Router();
 
-router.get("/", (req, res) => {
+const { User } = require('../../models');
+
+router.get('/', async (req, res) => {
+  // 카카오 로그인을 안해서 세션에 없으면 로그인을 하도록 루트 페이지로 이동시킴
   if (!req.session.kakao) {
-    //카카오 로그인을 안해서 세션에 없으면 로그인을 하도록 루트 페이지로 이동시킴
-    res.send(
-      "<script>alert('로그인이 필요한 기능입니다.');location.href='/';</script>"
-    );
-    return;
-  } else {
-    //카카오 로그인을 해서 세션에 kakao가 존재하면
-    res.render("myPage.html"); //
+    res.send("<script>alert('로그인이 필요한 기능입니다.');location.href='/';</script>");
     return;
   }
+
+  const {
+    kakao_account: {
+      email,
+    },
+  } = req.session.kakao;
+
+  // 카카오 로그인을 해서 세션에 kakao가 존재하면 user 객체와 함께 페이지 렌더링
+  const user = await User.findOne({ email });
+
+  res.render('myPage.html', user);
 });
 
 module.exports = router;
