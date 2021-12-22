5c9d363b6136167aeb4cf928d35ef0ff1817c5b6
Eunchae Ko <kec0130@gmail.com>
2021-12-23 00:47:18 +0900

    보호게시판 '목록으로' 버튼 추가

--- src/js/lostPets/index.js
+++ src/js/lostPets/index.js
@@ -1,9 +1,12 @@
 import { lostPetList, totalCount } from "./getLostPets.js";
 import { paintLostPets } from "./lostPets.js";
 import { paintLostPetDetail } from "./lostPetsDetail.js";
+import { createBackBtn } from "./backBtn.js";
 
 if (location.pathname === "/lostPets") {
   paintLostPets(lostPetList);
+  // 페이지네이션 추가
 } else {
   paintLostPetDetail(lostPetList);
+  createBackBtn();
 }
