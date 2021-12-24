2446090c04c51dd360b02fc4f778db4f206900d3
김동근 <kimdg0429@gmail.com>
2021-12-24 13:45:05 +0900

    웹팩 테스트

--- src/js/lostPets/lostMainPage.js
+++ src/js/lostPets/lostMainPage.js
@@ -1,7 +1,7 @@
 import { getLostPets } from "./getLostPets.js";
 import { paintLostPets } from "./lostPets.js";
 import { paintSearchForm } from "./searchForm.js";
-
+// 번들 만들 시 lostpetsMain페이지의 사실상 index.js의 역할
 let pageNo = 1;
 let limit = 10;
 const [lostPetList, totalCount] = await getLostPets(pageNo, limit);
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,19 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    // app: "./src/index.js",
-    // print: "./src/print.js",
+    lostMainPage: "./src/js/lostPets/lostMainPage.js",
+    lostDetailPage: "./src/js/lostPets/lostDetailPage.js",
   },
   devtool: "inline-source-map",
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ title: "Output Management" }),
   ],
-
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  experiments: {
+    topLevelAwait: true,
   },
 };
