e0e7d9c1b3bf7639e43938b431f71a0c313143be
김동근 <kimdg0429@gmail.com>
2021-12-24 14:24:38 +0900

    myPage webpack 번들(css+js) 적용

--- src/js/myPage/index.js
+++ src/js/myPage/index.js
@@ -4,15 +4,18 @@ import { user } from "./dummyUser.js";
 import { myPage } from "./myPage.js";
 import { changeName } from "./userChange.js";
 import { myPosts } from "./myPosts.js";
+import "../../style/myPage.css";
+import "../../style/index.css";
+import "../../style/myPetBoard.css";
 
 const run = () => {
-	window.addEventListener("DOMContentLoaded", () => {
-		mainTag();
-		myPage();
-		changeName();
-		myPosts(user);
-		myComments(user);
-	});
+  window.addEventListener("DOMContentLoaded", () => {
+    mainTag();
+    myPage();
+    changeName();
+    myPosts(user);
+    myComments(user);
+  });
 };
 
 run();
--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,21 @@ module.exports = {
   entry: {
     lostMainPage: "/src/js/lostPets/lostMainPage.js",
     lostDetailPage: "/src/js/lostPets/lostDetailPage.js",
+    myPage: "/src/js/myPage/index.js",
   },
   devtool: "inline-source-map",
   plugins: [new CleanWebpackPlugin()],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   experiments: {
     topLevelAwait: true,
