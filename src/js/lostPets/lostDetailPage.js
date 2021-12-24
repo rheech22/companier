f69cb91cc55ad855c2814f824950f23e64899144
Eunchae Ko <kec0130@gmail.com>
2021-12-24 11:14:01 +0900

    보호게시판 JS 파일 수정

---
+++ src/js/lostPets/lostDetailPage.js
@@ -0,0 +1,6 @@
+import { lostPetList } from "./lostMainPage.js";
+import { paintLostPetDetail } from "./lostPetsDetail.js";
+import { createBackBtn } from "./backBtn.js";
+
+paintLostPetDetail(lostPetList);
+createBackBtn();
