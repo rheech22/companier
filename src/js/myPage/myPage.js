8d34fb05c5d006d32b16bf645b1d4191fdaa17c3
hamelln <vusdus91@gmail.com>
2021-12-25 18:37:08 +0900

    style: myPage 스타일 수정

--- src/js/myPage/myPage.js
+++ src/js/myPage/myPage.js
@@ -13,8 +13,8 @@ function myPageTemplate(user) {
 	const email = document.querySelector(".myPage__profile__email");
 	const nickname = document.querySelector(".myPage__profile__nickname");
 	const createdAt = document.querySelector(".myPage__profile__createdAt");
-	email.innerHTML = `이메일 ${user.email}`;
-	nickname.innerHTML = `닉네임 ${user.nickname}`;
+	email.innerHTML = user.email;
+	nickname.innerHTML = user.nickname;
 	createdAt.innerHTML = `가입일 ${user.createdAt.split("-")[0]}년 ${
 		user.createdAt.split("-")[1]
 	}월 ${user.createdAt.split("-")[2].substr(0, 2)}일`;
@@ -30,7 +30,7 @@ function myPageTemplate(user) {
 			}
 			e.target.classList.add("is__on");
 			activeContent = e.target.getAttribute("href");
-			document.querySelector(activeContent).style.display = "block";
+			document.querySelector(activeContent).style.display = "flex";
 		});
 	}
 }
