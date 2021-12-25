07a106730154cb38371f7bb73ad64b89e4b62412
hamelln <vusdus91@gmail.com>
2021-12-25 14:06:31 +0900

    style: myPage 수정

--- src/js/myPage/main.js
+++ src/js/myPage/main.js
@@ -4,9 +4,7 @@ const mainTag = () => {
 	let mainTemplate = `
   <section class="myComments__list"></section>
   <div class="myComment__pages">
-    <div class="prev">이전 페이지</div>
     <ul class="pagination"></ul>
-    <div class="next">다음 페이지</div>
   </div>
     `;
 
