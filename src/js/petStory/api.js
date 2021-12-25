9ade264028a8c6875102d007e6012fdd092f8e23
김동근 <kimdg0429@gmail.com>
2021-12-25 14:49:42 +0900

    탈퇴한 유저 게시글 오류 수정

--- src/js/petStory/api.js
+++ src/js/petStory/api.js
@@ -3,6 +3,9 @@ const getPetStoryDetail = async (postId) => {
     const response = await fetch(`/api/posts/${postId}`);
     const result = await response.json();
 
+    if (!result.author) {
+      result.author = { nickname: "탈퇴한 유저" };
+    }
     return result;
   } catch (err) {
     console.log(err);
