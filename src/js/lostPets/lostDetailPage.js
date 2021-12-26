3ce87b34d386b085ad39f03199d4a4a2e7c8f4b5
Eunchae Ko <kec0130@gmail.com>
2021-12-26 18:50:21 +0900

    보호게시판 불필요한 주석 제거

--- src/js/lostPets/lostDetailPage.js
+++ src/js/lostPets/lostDetailPage.js
@@ -1,6 +1,6 @@
 import { lostPetList } from "./lostMainPage.js";
 import { paintLostPetDetail } from "./lostPetsDetail.js";
 import { createBackBtn } from "./backBtn.js";
-// 번들 만들 시 lostpetsdetail페이지의 사실상 index.js의 역할
+
 paintLostPetDetail(lostPetList);
 createBackBtn();
