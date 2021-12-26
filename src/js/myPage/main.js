191a0940efd4f24625455aafd0c1ca2bb49dea09
lth <vusdus91@gmail.com>
2021-12-26 12:14:11 +0900

    fix: myPage nickname 수정 시 새로고침이 안 되도록 수정

--- src/js/myPage/main.js
+++ src/js/myPage/main.js
@@ -1,14 +1,14 @@
 const mainTag = () => {
-	const main = document.querySelector("#myComments");
+  const main = document.querySelector("#myComments");
 
-	let mainTemplate = `
+  let mainTemplate = `
   <section class="myComments__list"></section>
   <div class="myComment__pages">
     <ul class="pagination"></ul>
   </div>
     `;
 
-	main.innerHTML = mainTemplate;
+  main.innerHTML = mainTemplate;
 };
 
 export { mainTag };
